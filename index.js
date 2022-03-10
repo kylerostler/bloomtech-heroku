e499f85739d0e977cbe8a5e50f948efa6ccea0a8
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-03-10 10:58:53 -0700

    built port

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
+const PORT = process.env.PORT || 9000
 const server = express()
 
 server.get('/', (req, res) => {
     res.end('<h1>hello world<h1>')
 })
 
-server.listen(9000, () => {
-    console.log('server is up on 9000')
+server.listen(PORT, () => {
+    console.log(process.env.MY_ENV)
+    console.log(process.env.QWERTY)
+    console.log(`server is up on ${PORT}, user is ${process.env.USERNAME}`)
 })
